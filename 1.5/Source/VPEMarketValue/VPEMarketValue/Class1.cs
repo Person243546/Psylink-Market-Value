83abb6a1989181f40db1c8b0683641c11eb096bf
Person243546 <72211112+Person243546@users.noreply.github.com>
2025-02-25 18:04:30 +0400

    Add files via upload

---
+++ 1.5/Source/VPEMarketValue/VPEMarketValue/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using RimWorld;
+using Verse;
+using HarmonyLib;
+
+namespace PsylinkMarketValue
+{
+    [StaticConstructorOnStartup]
+    public static class PsylinkMarketValue
+    {
+        static PsylinkMarketValue()
+        {
+            var harmony = new Harmony("Person.PsylinkMarketValue");
+            harmony.PatchAll();
+            Log.Message("Psylink Market Value Initialized");
+        }
+    }
+
+    [HarmonyPatch(typeof(PriceUtility))]
+    [HarmonyPatch("PawnQualityPriceOffset")]
+    class PsylinkMarketValuePatch
+    {
+        static void Postfix(ref float __result, ref Pawn pawn, ref StringBuilder explanation)
+        {
+            if (pawn.GetPsylinkLevel() != 0) 
+            {
+                float newvalueoffset = 0;
+                float PApriceOffset = HediffDefOf.PsychicAmplifier.priceOffset;
+                newvalueoffset = Convert.ToSingle(PApriceOffset * pawn.GetPsylinkLevel() - PApriceOffset);
+                explanation?.AppendLine("Psylink Level Offset: " + newvalueoffset.ToStringMoneyOffset());
+                //Log.Message(pawn.Name.ToStringFull + "'s psylink level offset: " + newvalueoffset);
+                __result += newvalueoffset;
+                
+            }
+        }
+    }
+}
